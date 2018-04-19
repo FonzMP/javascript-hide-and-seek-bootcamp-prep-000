b694f4856fc2e2e379c26c3466e510253ea96aa5
Alex Wilson <fonzmp@gmail.com>
2018-04-19 19:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   const variable = document.querySelector(selector);
   return variable
 }
+
+function nestedTarget() {
+  const variableTwo = document.querySelector('#app').querySelector('div#nested div div div div#target');
+  return variableTwo
+}
