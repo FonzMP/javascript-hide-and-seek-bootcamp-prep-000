0f58936c450e0d693d78c54279d702ab4bb2ee29
Alex Wilson <fonzmp@gmail.com>
2018-04-19 19:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function nestedTarget() {
   const variableTwo = document.getElementById('app').querySelector('div#nested div div div div.target');
   return variableTwo;
 }
+
+function increaseRankBy(n) {
+  const variableThree = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i  = 0; i < variableThree.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
+}
