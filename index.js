b1ed5ae8808d8a4a04026ecf1cb957c57d33f265
Alex Wilson <fonzmp@gmail.com>
2018-04-19 19:31:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  const variable = document.querySelector(selector);
+  return variable
+}
+
+function nestedTarget() {
+  const variableTwo = document.getElementById('app').querySelector('div#nested div div div div.target');
+  return variableTwo;
+}
+
+function increaseRankBy(n) {
+  const variableThree = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i  = 0; i < variableThree.length; i++) {
+    let content = variableThree[i].textContent;
+    variableThree[i].innerHTML =  parseInt(content) + n;
+  }
+}
+
+function deepestChild() {
+  const deepest = document.getElementById('app').querySelector('div#grand-node div div div div');
+  return deepest;
+}
